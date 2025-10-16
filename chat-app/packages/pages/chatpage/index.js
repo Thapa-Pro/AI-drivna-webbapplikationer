a09979724b3d0f76d4f2e5b553e76afbd185529f
Thapa-Pro <bachchan.k.t@gmail.com>
2025-10-16 14:44:08 +0200

    Sprakcoachen edited

--- chat-app/packages/pages/chatpage/index.js
+++
@@ -1,3 +0,0 @@
-export * from ".ui/"; // Re-export everything from the .ui directory
-// This ui folder contains all the presentational components related to the chat page.
-// It helps in keeping the code organized and modular.
